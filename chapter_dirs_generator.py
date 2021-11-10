48008315c771632d5cef79ff0d0eadd7bfc1b8f6
huaqing89 <huaqingxu89@gmail.com>
2021-11-10 22:55:08 +0800

    add init pyfiles

--- chapter_dirs_generator.py
+++ chapter_dirs_generator.py
@@ -1,4 +1,15 @@
 import os
+from pathlib import Path
+
+def gen_init_py(dir_path):
+    f = open(dir_path+"/__init__.py", "a")
+    f.write("create by xuhuaqing.")
+    f.close()
 
 for x in range(1, 17):
-    os.mkdir(f"chapter_{x}")
+    dir_path = f"chapter_{x}"
+    if Path(dir_path).is_dir():
+        gen_init_py(dir_path)
+    else:
+        os.mkdir(dir_path)
+        gen_init_py(dir_path)
